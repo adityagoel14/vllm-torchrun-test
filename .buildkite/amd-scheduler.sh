d6bb3d40f73f235a1d8c8de0f1aaccf73b75103b
adi <aditya.goel@amd.com>
2024-06-13 21:48:48 -0500

    fixed gpu scheduler path

--- .buildkite/amd-scheduler.sh
+++ .buildkite/amd-scheduler.sh
@@ -4,9 +4,9 @@ function execute_test {
     id=$1
     gpus=$2
 
-    gpu_list=$(python3 amd-gpu-scheduler.py assign $gpus)
+    gpu_list=$(python3 .buildkite/amd-gpu-scheduler.py assign $gpus)
 
-    # if [ -z "$GPU_IDS" ]; then
+    # if [ -z "$gpu_list" ]; then
     #   echo "Not enough GPUs available."
     #   return 0
     # fi
@@ -16,8 +16,8 @@ function execute_test {
 
     wait $AGENT_PID
 
-    python3 amd-gpu-scheduler.py release "$gpu_list"
-    echo "GPUs released: $GPU_IDS"
+    python3 .buildkite/amd-gpu-scheduler.py release "$gpu_list"
+    echo "GPUs released: $gpu_list"
 }
 
 cleanup() {
@@ -28,6 +28,7 @@ cleanup() {
 
 trap cleanup EXIT
 
+sudo apt-get install jq
 pip install FileLock
 
 while true; do
@@ -50,9 +51,9 @@ while true; do
     job_id=$(echo "$job" | jq -r '.id')
     job_gpus=$(echo "$job" | jq -r '.priority.number')
     echo "Job: ${job_label}"
-    if ! python3 amd-gpu-scheduler.py check $job_gpus; then
+    if ! python3 .buildkite/amd-gpu-scheduler.py check $job_gpus; then
         echo "Waiting for $job_gpus GPUs to become available..."
-        while ! python3 amd-gpu-scheduler.py check $job_gpus; do
+        while ! python3 .buildkite/amd-gpu-scheduler.py check $job_gpus; do
             sleep 10
         done
     fi
