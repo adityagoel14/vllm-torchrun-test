2b6718282aa604066467bdd61c37164f40135a7e
adi <aditya.goel@amd.com>
2024-06-15 11:45:55 -0500

    added priority mechanism, 2+ machine support

--- .buildkite/amd-scheduler.sh
+++ .buildkite/amd-scheduler.sh
@@ -5,48 +5,70 @@ function execute_test {
     gpus=$2
 
     gpu_list=$(python3 .buildkite/amd-gpu-scheduler.py assign $gpus) 
+    # Remove [] for env variable
     formatted_gpu_list=$(echo "$gpu_list" | jq -r '. | @csv' | tr -d '"')
 
-    # if [ -z "$gpu_list" ]; then
-    #   echo "Not enough GPUs available."
-    #   return 0
-    # fi
-
     echo "Adding env variable HIP_VISIBLE_DEVICES=${formatted_gpu_list}"
-    HIP_VISIBLE_DEVICES="${formatted_gpu_list}" buildkite-agent start --acquire-job=$id > /dev/null 2>&1
-    wait $AGENT_PID
 
+    # TO DO: don't redirect errors 
+
+    # Start a new Buildkite agent and pass in env variable to subprocess
+    HIP_VISIBLE_DEVICES="${formatted_gpu_list}" buildkite-agent start --acquire-job=$id > /dev/null 2>&1
+    
+    # After agents terminates free the GPUs it was using
     python3 .buildkite/amd-gpu-scheduler.py release "$gpu_list"
     echo "GPUs released: $gpu_list"
+}
+
+function is_job_still_scheduled {
+    id=$1
+    job_state=$(curl https://graphql.buildkite.com/v1 \
+        -H "Authorization: Bearer bkua_8b379ac0f6a511cc7715bbd48b02c938a6c26e77" \
+        -H "Content-Type: application/json" \
+        -d '{
+            "query": "{ job(uuid: \"'"$id"'\") { ... on JobTypeCommand { state } } }",
+            "variables": "{ }"
+        }' | jq -r '.data.job.state')
+
+    if [ "$job_state" == "SCHEDULED" ]; then
+        return 0  
+    else
+        return 1  
+    fi
 }
 
 cleanup() {
     echo "Cleaning up GPU state file..."
     rm -f /tmp/gpu_state.json
+    rm -f /tmp/gpu_lock
     echo "Cleanup complete. Exiting."
 }
 
 trap cleanup EXIT
 
+echo "--- Checking Dependences"
+
 sudo apt-get install jq
 pip install FileLock
 
-while true; do
-    job=$(curl https://graphql.buildkite.com/v1 \
+echo "--- Fetching Jobs" 
+
+jobs=$(curl https://graphql.buildkite.com/v1 \
     -H "Authorization: Bearer bkua_8b379ac0f6a511cc7715bbd48b02c938a6c26e77" \
     -H "Content-Type: application/json" \
     -d '{
-        "query": "{ build(slug: \"amd-11/torchrun-test-final/'"${BUILDKITE_BUILD_NUMBER}"'\") { jobs(first: 1, state: SCHEDULED) { edges { node { ... on JobTypeCommand { uuid label priority { number } } } } } } }",
+        "query": "{ build(slug: \"amd-11/torchrun-test-final/'"${BUILDKITE_BUILD_NUMBER}"'\") { jobs(first: 100, state: SCHEDULED) { edges { node { ... on JobTypeCommand { uuid label priority { number } } } } } } }",
         "variables": "{ }"
-    }' | jq -r '.data.build.jobs.edges[] | .node')
+    }' | jq -r '.data.build.jobs.edges | sort_by(.node.priority.number)[] | .node')
 
-    # Check if there are no more jobs in the queue
-    if [ -z "$job" ]; then
-        echo "No more Available Jobs" 
-        exit 0
-    fi
+# Convert into bash array
+jobs_array=($(echo "$jobs" | jq -s '.'))
 
-    # Scheduling job
+# Iterate through each job
+for job_json in "${jobs_array[@]}"; do
+
+    job=$(echo "$job_json" | jq '.')
+
     job_label=$(echo "$job" | jq -r '.label')
     job_id=$(echo "$job" | jq -r '.uuid')
     job_gpus=$(echo "$job" | jq -r '.priority.number')
@@ -59,6 +81,13 @@ while true; do
         continue
     fi
 
+    # Check if job has been taken by a different agent
+    if ! is_job_still_scheduled $job_id; then
+        echo "Skipping job as it is no longer scheduled -> $job_label"
+        continue
+    fi
+
+    # Check is there are sufficient GPUs available
     if ! python3 .buildkite/amd-gpu-scheduler.py check $job_gpus; then
         echo "Waiting for $job_gpus GPUs to become available..."
         while ! python3 .buildkite/amd-gpu-scheduler.py check $job_gpus; do
@@ -66,6 +95,7 @@ while true; do
         done
     fi
 
+    # Run test
     execute_test $job_id $job_gpus &
     sleep 10 
 
