6b545d95eb106d5af8e9bebae03c7fc68d9a71d8
adi <aditya.goel@amd.com>
2024-06-17 22:15:42 -0500

    putting tests in their own queue

--- .buildkite/amd-scheduler.sh
+++ .buildkite/amd-scheduler.sh
@@ -14,7 +14,7 @@ function execute_test {
     # TO DO: don't redirect errors 
 
     # Start a new Buildkite agent and pass in env variable to subprocess
-    HIP_VISIBLE_DEVICES="${formatted_gpu_list}" buildkite-agent start --acquire-job=$id #--queue amd-test > /dev/null #2>&1
+    HIP_VISIBLE_DEVICES="${formatted_gpu_list}" buildkite-agent start --acquire-job=$id --queue amd-test #> /dev/null #2>&1
     
     # After agents terminates free the GPUs it was using
     python3 .buildkite/amd-gpu-scheduler.py release "$gpu_list"
@@ -43,7 +43,7 @@ jobs=$(curl -s -S https://graphql.buildkite.com/v1 \
     -H "Authorization: Bearer bkua_8b379ac0f6a511cc7715bbd48b02c938a6c26e77" \
     -H "Content-Type: application/json" \
     -d '{
-        "query": "{ build(slug: \"amd-11/torchrun-test-final/'"${BUILDKITE_BUILD_NUMBER}"'\") { jobs(first: 100, state: SCHEDULED) { edges { node { ... on JobTypeCommand { uuid label priority { number } } } } } } }",
+        "query": "{ build(slug: \"amd-11/torchrun-test-final/'"${BUILDKITE_BUILD_NUMBER}"'\") { jobs(first: 100, state: SCHEDULED, agentQueryRules: \"queue=amd-test\") { edges { node { ... on JobTypeCommand { uuid label priority { number } } } } } } }",
         "variables": "{ }"
     }')
 
